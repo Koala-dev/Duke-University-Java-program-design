3476b76af96a8350fc8f0d9627d58012cd2562aa
Ryan Guo <guohao_lin@msn.com>
2017-05-16 18:53:50 -0700

    debugging part 2

---
+++ Find genes in DNA/debuggingPart2/findAbc.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class findAbc here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class findAbc
+{
+   public void findAbc(String input){
+       int index = input.indexOf("abc");
+       while (true){
+           if (index == -1 || index >= input.length() - 3){
+               break;
+           }
+           System.out.println("before calculated: " + index);
+           String found = input.substring(index+1, index+4);
+           System.out.println("index calculated " + index);
+           System.out.println(found);
+           index = input.indexOf("abc",index+3);
+           System.out.println("index after updating " + index);
+       }
+   }
+
+   public void test(){
+       //findAbc("abcd");
+       System.out.println("==========================");
+       findAbc("abcabcabcabca");
+       
+   }
+}
